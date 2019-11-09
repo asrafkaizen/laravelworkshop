6e33431a1f3bed4427ee2d515298d3a144dcb8af
asrafkaizen <asrafkaizen@gmail.com>
2019-11-09 12:32:20 +0800

    added more seeded data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,9 +18,35 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([
-            'name' => 'asyraf',
+            'name' => 'acap ipin',
             'email' => 'acap@gmail.com',
             'password' => bcrypt('acap123'),
         ]);
+        
+        DB::table('users')->insert([
+            'name' => 'asyraf arifin',
+            'email' => 'asrafkaizen@gmail.com',
+            'password' => bcrypt('acapacap'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'luqman mohidin',
+            'email' => 'luqmanhakim@gmail.com',
+            'password' => bcrypt('luqmankaslda'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'aqilah athirah',
+            'email' => 'aqilahathirah@gmail.com',
+            'password' => bcrypt('aqilahathirah'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'foo bar',
+            'email' => 'email@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        
     }
 }
