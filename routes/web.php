97ae0e753f8ac701e330aaa8118d94db49e6452d
asrafkaizen <asrafkaizen@gmail.com>
2019-11-09 11:43:11 +0800

    added login n register

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    echo "testing";
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
