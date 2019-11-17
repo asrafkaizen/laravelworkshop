6065230fc7b9bea8ba0f2a2c64d221f4f850e4ba
asrafkaizen <asrafkaizen@gmail.com>
2019-11-17 14:10:42 +0800

    edited the front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Training</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Acap's Laravel Training App
+                    <a href="{{ route('blogs') }}">
+                        Acap's Laravel App
+                    </a>
                 </div>
 
                 <div class="links">
